ef4ac31653e04e23bf9e9edb363af506a27056d2
David Hidalgo <89395017+davidleohidalgo@users.noreply.github.com>
2023-05-28 17:52:41 +0200

    Add files via upload

---
+++ Modulo_5/Modulo_5.js
@@ -0,0 +1,17 @@
+//Crea un archivo JS que contenga las siguientes líneas
+
+//Una variable que contenga tu altura en centímetros (entero)
+let alturacm =  180
+console.log(typeof alturacm)
+//Una variable que contenga tu altura en metros (número de coma flotante)
+let alturamts = 1.80
+console.log (typeof alturamts)
+//Una variable que contenga tu peso en kilogramos (número de coma flotante)
+let pesokg = 105.63
+//Una variable que contenga tu altura en metros redondeada hacia arriba
+let metrosUp = alturamts
+console.log(metrosUp.toFixed(0))
+//Una variable que contenga tu peso en kilogramos redondeado hacia abajo
+let kilogramosDown = pesokg
+console.log(kilogramosDown.toPrecision(4))
+//Una variable que contenga si "el máximo valor que se puede obtener en Javascript + 1" es igual al máximo valor que se puede obtener en Javascript
